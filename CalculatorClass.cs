5e364abc56074c711279d03e5f78203817dcb9d3
Expner <KDS4wexp@gmail.com>
2023-06-06 23:56:12 +0300

    v0.0

---
+++ CalculatorClass.cs
@@ -0,0 +1,33 @@
+﻿internal class Calculator
+{
+    public int Result { get; set; }
+    public int NumberOne { get; set; }
+    public int NumberTwo { get; set; }
+
+    public void Plus(int NumberOne, int NumberTwo)
+    {
+        Result = NumberOne + NumberTwo;
+        Console.WriteLine($"Result is {Result}");
+    }
+    public void Minus(int NumberOne, int NumberTwo)
+    {
+        Result = NumberOne - NumberTwo;
+        Console.WriteLine($"Result is {Result}");
+    }
+    public void Multyply(int NumberOne, int NumberTwo)
+    {
+        Result = NumberOne * NumberTwo;
+        Console.WriteLine($"Result is {Result}");
+    }
+    public void Divide(int NumberOne, int NumberTwo)
+    {
+        Result = NumberOne / NumberTwo;
+        Console.WriteLine($"Result is {Result}");
+    }
+
+    public Calculator(int NumberOne, int NumberTwo)
+    {
+        this.NumberOne = NumberOne;
+        this.NumberTwo = NumberTwo;
+    }
+}
